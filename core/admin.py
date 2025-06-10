f175d2d42fd8e9614ea9026ba028357bd9f62367
Therone Almadin <102451950+therone18@users.noreply.github.com>
2025-06-10 16:08:19 +0800

    API Endpoint Plan

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import ParkingLocation, ParkingSlot, Reservation
+
+admin.site.register(ParkingLocation)
+admin.site.register(ParkingSlot)
+admin.site.register(Reservation)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ParkingLocation(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    total_slots = models.PositiveIntegerField()
+    google_maps_url = models.URLField(blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class ParkingSlot(models.Model):
+    location = models.ForeignKey(ParkingLocation, on_delete=models.CASCADE)
+    slot_number = models.CharField(max_length=10)
+    is_available = models.BooleanField(default=True)
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    last_park_in = models.DateTimeField(null=True, blank=True)
+    last_park_out = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='active')
+    
+    #Car info
+    vehicle_make = models.CharField(max_length=50, blank=True, null=True)
+    vehicle_model = models.CharField(max_length=50, blank=True, null=True)
+    plate_number = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_type = models.CharField(max_length=20, blank=True, null=True)
