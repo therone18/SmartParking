a127ab2cc4804d4ffa9881829d12150dbd3c1e71
Therone Almadin <102451950+therone18@users.noreply.github.com>
2025-06-16 14:20:19 +0800

    test

--- smart_parking_frontend/src/pages/user_pages/Login.jsx
+++ smart_parking_frontend/src/pages/user_pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
     <div className="flex items-center justify-center min-h-screen bg-slate-50">
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-3xl font-bold mb-6 text-center text-indigo-800">
-          Be Smart, Start Parking.
+          Be Smart, Start Paring.
         </h2>
         <form onSubmit={handleSubmit} className="space-y-5">
           {/* Username Field */}
