a2a77874f868fc4703466d0684a9d8b72f8f8724
Therone Almadin <102451950+therone18@users.noreply.github.com>
2025-06-16 13:46:00 +0800

    Cors error fix #6
    Allowed all origins for now

--- smart_parking_backend/settings.py
+++ smart_parking_backend/settings.py
@@ -148,6 +148,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['smartparking-backend.onrender.com']  # Update this once deployed
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 CORS_ALLOWED_ORIGINS = [
     'https://smart-parking-peach.vercel.app',  
     'http://localhost:3000',
